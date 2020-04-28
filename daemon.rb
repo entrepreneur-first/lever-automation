d9a191b9acc2ba201149e5187e625935732ea7ad
Andy Young <andy@apexa.co.uk>
2020-04-28 17:51:14 +0100

    fix daemon disabled state

--- daemon.rb
+++ daemon.rb
@@ -3,14 +3,11 @@
 require 'daemons'
 require_relative 'app/controller'
 
-# check daemon is enabled
-exit if ENV['ENABLE_DAEMON'].nil? || ENV['ENABLE_DAEMON'].empty? || ['0', 'false'].include?(ENV['ENABLE_DAEMON'].downcase)
-
 controller = Controller.new
 
 Daemons.run_proc('lever_daemon.rb') do
   loop do
-    controller.process_opportunities
+    controller.process_opportunities unless ENV['ENABLE_DAEMON'].nil? || ENV['ENABLE_DAEMON'].empty? || ['0', 'false'].include?(ENV['ENABLE_DAEMON'].downcase)
     sleep(10)
   end
 end
