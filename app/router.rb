5de4d7bc20a9e441f47edda733bcc1ddb514746b
Andy Young <andy@apexa.co.uk>
2020-05-03 17:06:41 +0000

    [console] view candidate age

--- app/router.rb
+++ app/router.rb
@@ -8,6 +8,10 @@ class Router
     "Enter 'summarise', 'process', 'fix tags', 'check links', or '[view|feedback] <email>|<opportunity_id>' to view/process one candidate:"
   end
 
+  def self.interactive?
+    (ENV['LOG_FILE'] == '1') || (ENV['CONSOLE_ASYNC'] == '0')
+  end
+
   def self.route(command)  
     return if command == ''
     finished_successfully = false
@@ -15,7 +19,7 @@ class Router
     controller = Controller.new
     controller.log.verbose
     
-    controller.log.log('Command: ' + command)
+    controller.log.log('Command: ' + command) unless self.interactive?
 
     case command
     when 'summarise'
@@ -55,6 +59,8 @@ class Router
         os = [controller.client.get_opportunity(key, {expand: controller.client.OPP_EXPAND_VALUES})].reject{|o| o.nil?}
       end
 
+      puts "\n" if self.interactive?
+
       case command
       when 'view'
         puts JSON.pretty_generate(os)
@@ -64,6 +70,8 @@ class Router
         os.each{ |opp| puts JSON.pretty_generate controller.client.get_paged_result("#{API_URL}opportunities/#{opp['id']}/notes", {}, 'notes') }
       when 'tidy_bot_notes'
         os.each{ |opp| controller.tidy_opp_bot_notes(opp) }
+      when 'age'
+        os.each{ |opp| puts "#{opp['id']}: #{opp['lastInteractionAt'] - opp['createdAt']}" }
       else
         os.each{ |opp| controller.process_opportunity(opp) }
       end
@@ -72,6 +80,7 @@ class Router
     finished_successfully = true
     
     ensure
+      puts "\n" if self.interactive?
       controller.log.log("#{finished_successfully ? 'Finished' : 'Aborted'} command: " + command)
   end
 
