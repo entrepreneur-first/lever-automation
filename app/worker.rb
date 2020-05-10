cb352d47de201711b2067eec0913e524c41c1a72
Andy Young <andy@apexa.co.uk>
2020-05-10 18:32:47 +0100

    [sidekiq] default to test queue

--- app/worker.rb
+++ app/worker.rb
@@ -5,7 +5,7 @@ require_relative 'router'
 
 class Worker
   include Sidekiq::Worker
-  sidekiq_options queue: ENV['APP_ENV'] || 'default'
+  sidekiq_options queue: ENV['APP_ENV'] || 'test'
 
   def perform(command)
     Router.route(command)
