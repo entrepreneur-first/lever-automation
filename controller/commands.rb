22e601afed2e0acc9b9a37db36d4dc79ebaac50a
Andy Young <andy@apexa.co.uk>
2020-05-14 00:10:03 +0100

    [slackbot] fix

--- controller/commands.rb
+++ controller/commands.rb
@@ -250,7 +250,7 @@ module Controller_Commands
           "type": "section",
           "text": {
             "type": "mrkdwn",
-            "text": "#{opp['archived'].nil? ? '👤 ' : '👻 '}<#{opp['urls']['show']}|*#{opp['name']* - view on Lever}>#{opp['archived'].nil? ? '' : ' [archived]'}" \
+            "text": "#{opp['archived'].nil? ? '👤 ' : '👻 '}<#{opp['urls']['show']}|*#{opp['name']}* - view on Lever>#{opp['archived'].nil? ? '' : ' [archived]'}" \
               "#{opp_data['application__posting__text'] ? "\n" + opp_data['application__posting__text'] : ''}" \
               "\n*Email#{opp['emails'].size > 1 ? 's' : ''}:* #{opp['emails'].join(', ')}" \
               "#{opp['links'].select{|l| l.include?('linkedin.com')}.any? ? "\n*LinkedIn:* #{opp['links'].select{|l| l.include?('linkedin.com')}.join(', ')}" : ''}" \
