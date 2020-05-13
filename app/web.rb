98bfb9ea70f2009b4d68ed8235f5bef779c03438
Andy Young <andy@apexa.co.uk>
2020-05-14 00:14:56 +0100

    [slackbot] format help

--- app/web.rb
+++ app/web.rb
@@ -7,7 +7,7 @@ require_relative '../controller/controller'
 
 use SlackAuthorizer
 
-HELP_RESPONSE = 'Use `/lever` to look up a candidate in Lever by name, email or link. Example: `/lever Dolly Parton`, `/lever dolly@parton.com`, `/lever linkedin.com/in/dollyparton`'.freeze
+HELP_RESPONSE = 'Use `/lever` to look up a candidate in Lever by name, email or link. \nExamples: \n - `/lever Dolly Parton` \n- `/lever dolly@parton.com` \n- `/lever linkedin.com/in/dollyparton`'.freeze
 
 VALID_LOOKUP_EXPRESSION = /^(.+)/
 
