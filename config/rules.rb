4b778103be917a3b93b5220b92ff5112751e78c3
vit-ef <64260126+vit-ef@users.noreply.github.com>
2020-07-14 10:47:14 +0100

    Update rules.rb

--- config/rules.rb
+++ config/rules.rb
@@ -376,9 +376,13 @@ class Rules < BaseRules
     if (opp['tags'] & ['Software', 'Hardware']).any?
       value = opp['tags'].include?('Software') ? 'software' : 'hardware'
       # set value for most feedback form if not already present
-      if result[:has_app_review]
-        result[:app_review_software_hardware] ||= value
-      elsif result[:has_coffee]
+    
+      # remove app review from list of feedback forms to check 
+      # if result[:has_app_review]
+      #  result[:app_review_software_hardware] ||= value
+      #els
+      
+      if result[:has_coffee]
         result[:coffee_software_hardware] ||= value
       elsif result[:has_pre_coffee_screen]
         result[:pre_coffee_screen_software_hardware] ||= value
